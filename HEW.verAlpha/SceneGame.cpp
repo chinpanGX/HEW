82e5ec5b6f5e0229845fdb9751bd4ef929ffbe08
chinpanGX <52692512+chinpanGX@users.noreply.github.com>
2019-10-30 01:37:42 +0900

    SceneGameの追加、SceneManagerの修正

---
+++ HEW.verAlpha/SceneGame.cpp
@@ -0,0 +1,38 @@
+/*==================================
+
+	[SceneGame.cpp]
+	Author : 出合翔太
+
+===================================*/
+
+#include "SceneGame.h"
+#include "SceneManager.h"
+#include "input.h"
+
+//	デストラクタ
+SceneGame::~SceneGame()
+{
+}
+
+//	ゲームの初期化処理
+void SceneGame::Init()
+{
+
+}
+
+//	ゲームの終了処理
+void SceneGame::Uninit()
+{
+
+}
+
+//	ゲームの更新処理
+void SceneGame::Update()
+{
+
+}
+
+//	ゲームの描画処理
+void SceneGame::Draw()
+{
+}
--- HEW.verAlpha/SceneManager.cpp
+++ HEW.verAlpha/SceneManager.cpp
@@ -4,25 +4,43 @@
 	Author : 出合翔太
 
 ===============================================*/
-
+#include "main.h"
+#include "input.h"
+#include "SceneGame.h"
 #include "SceneManager.h"
 
+//グローバル変数
+SceneBase			*SceneManager::scene[SCENE_NUMBER];
+SCENE_STATE			SceneManager::scenestate;
+LPDIRECT3DDEVICE9	SceneManager::p3DDevice;
+
+//	初期化処理
 void SceneManager::Init()
 {
+	p3DDevice = GetD3DDevice();	//	デバイスの取得
+	scene[0] = new SceneGame;	//	シーンのインスタンス生成
+	scenestate = SCENE_GAME;	//	初期シーンの設定
+	scene[scenestate]->Init();	//	初期シー㎜の初期化
 }
 
+//	終了処理
 void SceneManager::Uninit()
 {
+	scene[0]->Uninit();
+	delete scene[0];
 }
 
+//	更新処理
 void SceneManager::Update()
 {
 }
 
+//	描画処理
 void SceneManager::Draw()
 {
 }
 
+//	シーン遷移処理
 void SceneManager::ChangeSceneState()
 {
 }
