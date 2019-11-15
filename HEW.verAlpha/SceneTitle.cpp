3d36c33620fbbacf2df6aeab328305190f64e5bf
chinpanGX <52692512+chinpanGX@users.noreply.github.com>
2019-11-15 22:48:35 +0900

    ゲームに必要な各シーンのクラスを追加→タイトル、モード選択、チュートリアル、リザルト

---
+++ HEW.verAlpha/SceneTitle.cpp
@@ -0,0 +1,24 @@
+/*===================================================
+
+	[SceneTitle.cpp]
+	Author : èoçá„ƒëæ
+
+====================================================*/
+
+#include "SceneTitle.h"
+
+void SceneTitle::Init()
+{
+}
+
+void SceneTitle::Uninit()
+{
+}
+
+void SceneTitle::Update()
+{
+}
+
+void SceneTitle::Draw()
+{
+}
