d66f9909f929a2227855f7ebfb498484c3611249
chinpanGX <52692512+chinpanGX@users.noreply.github.com>
2019-12-04 22:29:05 +0900

    カラー設定用描画関数

--- HEW/sprite.cpp
+++ HEW/sprite.cpp
@@ -135,6 +135,34 @@ void Sprite::Draw(TextureIndex texture_index, float dx, float dy, int tx, int ty
     m_Device->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, vertexes, sizeof(VERTEX_2D));
 }
 
+//	描画 (引数：テクスチャ、カラー設定、X座標、Y座標、テクスチャX座標、テクスチャY座標、テクスチャの横幅、テクスチャの縦幅)
+void Sprite::Draw(TextureIndex texture_index, D3DCOLOR color,float dx, float dy, int tx, int ty, int tw, int th)
+{
+	m_Device = GetD3DDevice();
+	if (!m_Device) return;
+
+	float w = (float)Texture_GetWidth(texture_index);
+	float h = (float)Texture_GetHeight(texture_index);
+
+	float u[2], v[2];
+	u[0] = (float)tx / w;
+	v[0] = (float)ty / h;
+	u[1] = (float)(tx + tw) / w;
+	v[1] = (float)(ty + th) / h;
+
+	VERTEX_2D vertexes[] =
+	{
+		{ D3DXVECTOR4(dx - MINA, dy - MINA, 0.0f, 1.0f),color, D3DXVECTOR2(u[0], v[0]) },
+		{ D3DXVECTOR4(dx + tw - MINA, dy - MINA, 0.0f, 1.0f),color, D3DXVECTOR2(u[1], v[0]) },
+		{ D3DXVECTOR4(dx - MINA, dy + th - MINA, 0.0f, 1.0f),color, D3DXVECTOR2(u[0], v[1]) },
+		{ D3DXVECTOR4(dx + tw - MINA, dy + th - MINA, 0.0f, 1.0f), color, D3DXVECTOR2(u[1], v[1]) },
+	};
+
+	m_Device->SetFVF(FVF_VERTEX2D);
+	m_Device->SetTexture(0, Texture_GetTexture(texture_index));
+	m_Device->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, vertexes, sizeof(VERTEX_2D));
+}
+
 //	描画 (引数：テクスチャ、X座標、Y座標、テクスチャX座標、テクスチャY座標、テクスチャの横幅、テクスチャの縦幅、テクスチャの中心座標、拡大縮小、回転)
 void Sprite::Draw(TextureIndex texture_index, float dx, float dy, int tx, int ty, int tw, int th, float cx, float cy, float sx, float sy, float rotation)
 {
@@ -184,47 +212,4 @@ void Sprite::Draw(TextureIndex texture_index, float dx, float dy, int tx, int ty
 	m_Device->SetFVF(FVF_VERTEX2D);
 	m_Device->SetTexture(0, Texture_GetTexture(texture_index));
 	m_Device->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, vertexes, sizeof(VERTEX_2D));
-}
-
-//	3D描画（引数：テクスチャ、X座標、Y座標、Z座標、）
-void Sprite::Draw(TextureIndex Texture, float pos_x,float pos_y,float pos_z)
-{
-	m_Device = GetD3DDevice();
-
-	D3DXMATRIX mtxScl, mtxRot, mtxTranslate;
-
-	//	ワールド変換（ワールドマトリクスの初期化）0
-	D3DXMatrixIdentity(&m_mtxWorld);
-	D3DXMatrixScaling(&mtxScl, 1, 1, 1);
-	D3DXMatrixMultiply(&m_mtxWorld, &m_mtxWorld, &mtxScl);					//　引数は2番目と3番目の計算結果を1番目に入れる
-	D3DXMatrixRotationYawPitchRoll(&mtxRot, 0, 0, 0);
-	D3DXMatrixMultiply(&m_mtxWorld, &m_mtxWorld, &mtxRot);
-	D3DXMatrixTranslation(&mtxTranslate, pos_x, pos_y, pos_z);
-	D3DXMatrixMultiply(&m_mtxWorld, &m_mtxWorld, &mtxTranslate);
-
-	// ワールドマトリクスの設定
-	m_Device->SetTransform(D3DTS_WORLD, &m_mtxWorld);
-		
-	//	頂点フォーマットの設定
-	m_Device->SetFVF(FVF_VERTEX3D);
-
-	//テクスチャの設定
-	m_Device->SetTexture(0, Texture_GetTexture(Texture));
-
-	//----------------------------------------------------------------------------------------------
-	//	  DrawPrimitiveUpとDrawRimitiveの違い
-	//"		DrawPrimitiveUpの説明	->	板ポリなど頂点が少ないオブジェクトで使う
-	//!			１．DrawPrimitiveUpが頂点情報を通常のメモリに置く -> 処理が遅くなる
-	//!			２．LockとUnlockを使わなくていい	
-	//"		DrawPrimitiveの説明		->	頂点が多いモデルファイルなどを描画するときに使う
-	//!			１．頂点情報がVRAM上に置かれる　-> 処理速度が速い
-	//!			２．LockとUnlockが必要になる	-> パフォーマンスが低下 ->なるべく少なくしたい
-	//----------------------------------------------------------------------------------------------
-
-	//	自分で作った頂点バッファを描画（メモリの確保、解放をしなければならない　-> 多用すると遅くなる）
-	//DrawPrimitive (第1引数：プリミティブを指定、第2引数：描画を開始する頂点を指定、第3引数：使用するプリミティブの数を指定)n 
-	//m_Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, POLYGON_NUM);
-	m_Device->DrawIndexedPrimitive(D3DPT_TRIANGLESTRIP, 0, 0, 20, 0, 12);
-
-	m_Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 2);
 }
