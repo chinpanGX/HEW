753997256d6ea23e3f034dd87e32fc5de961a8ee
chinpanGX <52692512+chinpanGX@users.noreply.github.com>
2019-12-09 01:37:10 +0900

    授業と同じやり方のマップ描画

---
+++ HEW/Field.h
@@ -0,0 +1,28 @@
+/*============================================
+	
+	[Field.h]
+	Author :　出合翔太
+
+============================================*/
+
+#pragma once
+#include "main.h"
+
+class Field
+{
+private:
+	static LPDIRECT3DTEXTURE9	m_pTextureModel;	//	テクスチャへのポインタ
+	static LPD3DXMESH			m_pMeshModel;		//	メッシュ情報へのポインタ
+	static LPD3DXBUFFER			m_pBuffMatModel;	//	マテリアル情報へのポインタ
+	static DWORD				m_nNumMatModel;		//	マテリアル情報の総数
+	static LPDIRECT3DDEVICE9	m_pDevice;			//	デバイス
+	D3DXMATRIX					m_mtxWorldModel;	//	ワールドマトリックス
+	D3DXVECTOR3					m_position;			//	位置
+	D3DXVECTOR3					m_scale;			//	スケール
+	D3DXVECTOR3					m_angle;			//	回転
+public:
+	HRESULT Init(D3DXVECTOR3 Pos, D3DXVECTOR3 Scale, D3DXVECTOR3 Angle);
+	void Uninit();
+	void Draw();
+};
+
