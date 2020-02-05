daf962dbae8ab1e498c2ed04cac1945b8d875ecc
chinpanGX <52692512+chinpanGX@users.noreply.github.com>
2020-02-05 10:37:58 +0900

    no message

--- HEWマスター版/Score.cpp
+++ HEWマスター版/Score.cpp
@@ -38,6 +38,6 @@ void Draw(float x, float y, int n)
 {
 	Sprite m_sprite;
 	if (n < 0 || n > 9) return;
-	m_sprite.Draw(TEXTURE_UI, x, y,90 * n, 140, 90, 110,45,55);
+	m_sprite.Draw(TEXTURE_UI, x, y,90 * n, 140, 90, 110,45,55,2.0f,2.0f,0.0f);
 }
 
